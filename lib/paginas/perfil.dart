eb59135d1d1f2a6eab5869122efe4b0f8985e429
Ericsoubud <erickkpela@gmail.com>
2024-12-06 20:14:56 -0300

    Image piker

--- lib/paginas/perfil.dart
+++ lib/paginas/perfil.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Perfil extends StatefulWidget {
   const Perfil({super.key});
@@ -8,6 +11,19 @@ class Perfil extends StatefulWidget {
 }
 
 class _PerfilState extends State<Perfil> {
+  final image_piker = ImagePicker();
+  File? image_file;
+
+  pick(ImageSource source) async{
+    final pickedFile = await image_piker.pickImage(source: source);
+
+    if(pickedFile != null){
+      setState(() {
+        image_file = File(pickedFile.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,19 +43,52 @@ class _PerfilState extends State<Perfil> {
       backgroundColor: const Color.fromARGB(255, 255, 245, 233),
       body: ListView(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Column(
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                CircleAvatar(
-                  radius: 80,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Stack(
+                      children: [
+                        CircleAvatar(
+                          radius: 75,
+                          backgroundColor: Colors.grey[200],
+                          child: CircleAvatar(
+                            radius: 65,
+                            backgroundColor: Colors.grey[300],
+                            backgroundImage:
+                                image_file != null ? FileImage(image_file!) : null,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 5,
+                          right: 5,
+                          child: CircleAvatar(
+                            backgroundColor: Colors.grey[200],
+                            child: IconButton(
+                              onPressed: _showOpcoesBottomSheet,
+                              icon: Icon(
+                                Icons.edit,
+                                color: Colors.grey[400],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
+              ],
+            ),
                 SizedBox(height: 10,),
-                Text(
-                  "Placeholder",
-                  style: TextStyle(
-                    fontSize: 23,
-                    fontWeight: FontWeight.w500
+                Center(
+                  child: Text(
+                    "Placeholder",
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.w500
+                    ),
                   ),
                 ),
                 SizedBox(height: 50),
@@ -68,12 +117,88 @@ class _PerfilState extends State<Perfil> {
                   color: const Color.fromARGB(255, 214, 205, 194),
                   borderRadius: BorderRadius.circular(40.0),
                    )
-                )
+                ),
               ],
             ),
-          )
-        ],
-      ),
+          );
+  }
+  void _showOpcoesBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.grey[200],
+                  child: Center(
+                    child: Icon(
+                      Icons.photo,
+                      color: Colors.grey[500],
+                    ),
+                  ),
+                ),
+                title: Text(
+                  'Galeria',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  // Buscar imagem da galeria
+                  pick(ImageSource.gallery);
+                },
+              ),
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.grey[200],
+                  child: Center(
+                    child: Icon(
+                      Icons.camera_alt,
+                      color: Colors.grey[500],
+                    ),
+                  ),
+                ),
+                title: Text(
+                  'Câmera',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  // Fazer foto da câmera
+                  pick(ImageSource.camera);
+                },
+              ),
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.grey[200],
+                  child: Center(
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.grey[500],
+                    ),
+                  ),
+                ),
+                title: Text(
+                  'Remover',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  // Tornar a foto null
+                  setState(() {
+                    image_file = null;
+                  });
+                },
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
+
 }
+
